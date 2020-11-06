347f062e1d493a2136c04d85750bb48b4701c0d0
Ralf Geisthardt <Ralf.Geisthardt@gfk.com>
2020-11-06 10:00:22 +0100

    CTP-3739 ->add cookie pro publisher to demo website

--- S3upload.php
+++ S3upload.php
@@ -52,6 +52,8 @@ $files = [
     $pathS3.'/video.html',
     $pathS3.'/html5video.html',
     $pathS3.'/html5videoctv.html',
+    $pathS3.'/html5videotcf.html',
+    $pathS3.'/html5videotcfCookiePro.html',
     $pathS3.'/youtube-video.html',
     $pathS3.'/sui-connector-test.html',
     $pathS3.'/touchpoint-test.html',
