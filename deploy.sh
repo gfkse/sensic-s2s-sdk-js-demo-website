309db9ae699eb6abc5d408a9cb79943ffe89ce10
Spengreb <spengreb@gmail.com>
2020-11-19 12:22:23 +0100

    CTP-3800: Add cloudfront invalidation to deploy, add s3 sync

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 s3_bucket=sensic-demo
 region=eu-central-1
 target_folder=$1
+cloudfront_dist_id=$2
 
 # Copy all files (except .tmpl.html) from ./website/
-aws s3 cp ./website/* s3://${s3_bucket}/${target_folder}/ --exclude *.tmpl.html --region ${region} --recursive
+aws s3 sync website s3://${s3_bucket}/${target_folder}/ --exclude *.tmpl.html --region ${region}
+
+aws cloudfront create-invalidation --distribution-id ${cloudfront_dist_id} --paths "/*" 
