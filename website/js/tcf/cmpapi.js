edc7d7abe96b87b94ce42be44422bf193d498135
Scheller, Robin (GfK) <Robin.Scheller@gfk.com>
2021-02-17 14:58:03 +0100

    CTP-3739 ->add radios for tcf publisher restriction

--- website/js/tcf/cmpapi.js
+++ website/js/tcf/cmpapi.js
@@ -5,6 +5,10 @@ const purpose1 = new URLSearchParams(window.location.search).getAll("purpose1");
 const purpose7 = new URLSearchParams(window.location.search).getAll("purpose7");
 const purpose8 = new URLSearchParams(window.location.search).getAll("purpose8");
 const purpose9 = new URLSearchParams(window.location.search).getAll("purpose9");
+const restriction1 = new URLSearchParams(window.location.search).get("restriction1");
+const restriction7 = new URLSearchParams(window.location.search).get("restriction7");
+const restriction8 = new URLSearchParams(window.location.search).get("restriction8");
+const restriction9 = new URLSearchParams(window.location.search).get("restriction9");
 
 window.__tcfapi = async (command, version, callback) => {
     if (command !== "addEventListener" || version !== 2) {
@@ -24,11 +28,31 @@ window.__tcfapi = async (command, version, callback) => {
     baseTcObject.purpose.legitimateInterests[7] = purpose7.includes("legitimateInterest");
     baseTcObject.purpose.legitimateInterests[8] = purpose8.includes("legitimateInterest");
     baseTcObject.purpose.legitimateInterests[9] = purpose9.includes("legitimateInterest");
+    // baseTcObject.publisher.restrictions[9] = purpose9.includes("legitimateInterest");
+    //add state 0or1or2
+
+    if (restriction1 === "0" || restriction1 === "1" || restriction1 === "2"){
+
+        baseTcObject.publisher.restrictions[1] = {758: restriction1 * 1}
+    }
+    if (restriction7 === "0" || restriction7 === "1" || restriction7 === "2"){
+        baseTcObject.publisher.restrictions[7] = {758: restriction7 * 1}
+    }
+    if (restriction8 === "0" || restriction8 === "1" || restriction8 === "2"){
+        baseTcObject.publisher.restrictions[8] = {758: restriction8 * 1}
+    }
+    if (restriction9 === "0" || restriction9 === "1" || restriction9 === "2") {
+        baseTcObject.publisher.restrictions[9] = {758: restriction9 * 1}
+    }
     callback(baseTcObject, true);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
     window.document.getElementById(`optin_${optin || "undefined"}`).checked = true;
+    window.document.getElementById(`publisher_restrictions_${restriction1 || "undefined"}_purpose_1`).checked = true;
+    window.document.getElementById(`publisher_restrictions_${restriction7 || "undefined"}_purpose_7`).checked = true;
+    window.document.getElementById(`publisher_restrictions_${restriction8 || "undefined"}_purpose_8`).checked = true;
+    window.document.getElementById(`publisher_restrictions_${restriction9 || "undefined"}_purpose_9`).checked = true;
     window.document.getElementById(`gdpr_${gdpr || "noapplies"}`).checked = true;
     vendor.forEach(value => window.document.getElementById(`vendor_${value}`).checked = true);
     purpose1.forEach(value => window.document.getElementById(`purpose1_${value}`).checked = true);
