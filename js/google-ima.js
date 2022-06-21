3c2a749f0e4e93c1ef53b7b2ba6a4c860343af56
Martin-Bruntsch <49907710+Martin-Bruntsch@users.noreply.github.com>
2022-06-21 15:51:42 +0200

    new demo website version has been released

--- js/google-ima.js
+++ js/google-ima.js
@@ -7,6 +7,7 @@ var adsManager;
 var adRunning = false;
 var video;
 adContainer.style.display = 'none';
+var playAdButton = document.getElementById('play_ad');
 
 function initGoogleIma(videoElement, tagUrl) {
   video = videoElement;
@@ -16,7 +17,7 @@ function initGoogleIma(videoElement, tagUrl) {
   var adsRequest = new google.ima.AdsRequest();
   // real live ad
   // adsRequest.adTagUrl = '//ad13.adfarm1.adition.com/banner?wpt=X&ts=1647952877&sid=4420431&p[stype:vod,scat:doku-reportage,scatid:1173,spro:der-talentierte-herr-hessenthaler,sproid:13893759,episodeid:14128205,duration:2721000,advertisingtags:null,oon-ds-ads:undefined,platform:web,test:false,viewport:tablet,orientation:landscape,user-agent:orf-tvthek-mozilla-50-windows-nt-100-win64-x64-applewebkit-53736-khtml-like-gecko-chrome-990484451-safari-53736,ctype:12]';
-  adsRequest.adTagUrl = '//pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpremidpost&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&cmsid=496&vid=short_onecue&correlator='
+  adsRequest.adTagUrl = '//pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpremidpost&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&cmsid=496&vid=short_onecue&correlator=';
   if (tagUrl) {
     adsRequest.adTagUrl = tagUrl;
   }
@@ -27,15 +28,22 @@ function initGoogleIma(videoElement, tagUrl) {
   adsRequest.linearAdSlotHeight = video.clientHeight;
   adsRequest.nonLinearAdSlotWidth = video.clientWidth;
   adsRequest.nonLinearAdSlotHeight = video.clientHeight / 3;
-  video.addEventListener('click', function() {
-    adDisplayContainer.initialize();
-  });
-  // Pass the request to the adsLoader to request ads
-  video.addEventListener('playing', function() {
-    setTimeout(() => {
+  if (playAdButton) {
+    playAdButton.addEventListener('click', function() {
+      adDisplayContainer.initialize();
       adsLoader.requestAds(adsRequest);
-    }, 3000);
-  });
+    });
+
+  } else {
+    video.addEventListener('click', function() {
+      adDisplayContainer.initialize();
+    });
+    video.addEventListener('playing', function() {
+      setTimeout(() => {
+        adsLoader.requestAds(adsRequest);
+      }, 3000);
+    });
+  }
   addEvents();
 }
 
